d7af99c0e13a38c9e4efb884fd717205b0bf9c21
chizhang <andreachizhang@yahoo.com>
2023-06-02 14:51:34 +0200

    some more notes

---
+++ script/1_gen_simple.R
@@ -0,0 +1,22 @@
+# genrate some data without thinking too much about the layers
+
+# types of outcomes: continuous, binary, survival
+# process
+
+# one group ----
+x <- rnorm()
+
+# binary dist
+# survival dist
+
+
+
+
+# two groups ----
+# (e.g. sex)
+
+
+
+# more groups ----
+
+
